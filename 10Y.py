7bfaed7c23d8e1ce63e8f78b95b5295746dd2300
watersalamander <118361497+watersalamander@users.noreply.github.com>
2024-12-05 17:43:59 +0700

    Update 10Y.py

--- 10Y.py
+++ 10Y.py
@@ -3,6 +3,10 @@ import streamlit as st
 st.set_page_config(page_title="Allocation",
                    layout="centered")
 
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.image("https://moneyexpo.net/wp-content/uploads/2024/04/Merkel.jpg", use_column_width=True)
+  
 st.title("Workshop 1")
 
 gold = st.number_input('Percentage in Gold',
