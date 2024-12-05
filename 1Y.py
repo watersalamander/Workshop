9ef14d4fcafd1754bea1b7e9b24fffb365145f73
watersalamander <118361497+watersalamander@users.noreply.github.com>
2024-12-05 17:29:55 +0700

    Create 1Y.py

---
+++ 1Y.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+st.set_page_config(page_title="Allocation",
+                   layout="centered")
+
+st.title("Workshop 1")
+
+gold = st.number_input('Percentage in Gold',
+                       min_value=0.0,
+                       max_value=100.0,
+                       value=20.0,
+                       step=10.0)
+bitcoin = st.number_input('Percentage in Bitcoin',
+                        min_value=0.0,
+                        max_value=100.0,
+                        value=20.0,
+                        step=10.0)
+reits = st.number_input('Percentage in REITs',
+                        min_value=0.0,
+                        max_value=100.0,
+                        value=20.0,
+                        step=10.0)
+stock = st.number_input('Percentage in Stock',
+                        min_value=0.0,
+                        max_value=100.0,
+                        value=20.0,
+                        step=10.0)
+bond = st.number_input('Percentage in Bond',
+                        min_value=0.0,
+                        max_value=100.0,
+                        value=20.0,
+                        step=10.0)
+
+button = st.button('Calculate')
+
+if button:
+    total_return = ((gold*29.52)+(bitcoin*143.95)+(reits*12.34)+(stock*22.08)+(bond*4.45))*0.01
+    st.write("1-Year Return: ", total_return, "%")
+
+    volatility = ((gold*15)+(bitcoin*72.9)+(reits*22.7)+(stock*19.6)+(bond*6.4))*0.01
+    st.write("1-Year Volatility: ", volatility, "%")
